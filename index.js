a13c095c01f6c3c7f5a41f0459e7fdcdd8665c21
enaprz <enasheccid7@gmail.com>
2023-10-16 13:27:45 -0700

    completed lab

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+let result;
+   if (string === string.toUpperCase()){ 
+    result = "YES INDEED!"
+   }
+   if (string === string.toLowerCase()){
+    result = "I can't hear you!"
+   }
+   if (string === "Let's have dinner together!"){
+    result = "I would love to!"
+   }
+   return result;
+    }
